3024aeb586eb68908d4b9766272cb4f52446c98c
yupeng <ypyupeng@vip.qq.com>
2016-02-26 17:15:55 +0800

    语法练习

--- src/com/yp/basic/GrammarTest.py
+++ src/com/yp/basic/GrammarTest.py
@@ -4,25 +4,74 @@ Created on 2016年2月26日
 
 @author: yupeng
 '''
-#python 表示符
-#在python里，标识符有字母、数字、下划线组成。
-#在python中，所有标识符可以包括英文、数字以及下划线（_），但不能以数字开头。
-#python中的标识符是区分大小写的。
-#以下划线开头的标识符是有特殊意义的。以单下划线开头（_foo）的代表不能直接访问的类属性，需通过类提供的接口进行访问，不能用"from xxx import *"而导入；
-#以双下划线开头的（__foo）代表类的私有成员；以双下划线开头和结尾的（__foo__）代表python里特殊方法专用的标识，如__init__（）代表类的构造函数。
-
+# python 表示符
+# 在python里，标识符有字母、数字、下划线组成。
+# 在python中，所有标识符可以包括英文、数字以及下划线（_），但不能以数字开头。
+# python中的标识符是区分大小写的。
+# 以下划线开头的标识符是有特殊意义的。以单下划线开头（_foo）的代表不能直接访问的类属性，需通过类提供的接口进行访问，不能用"from xxx import *"而导入；
+# 以双下划线开头的（__foo）代表类的私有成员；以双下划线开头和结尾的（__foo__）代表python里特殊方法专用的标识，如__init__（）代表类的构造函数。
 if True:
     print "True"
 else:
     print "False"
 
-counter = 100 # 赋值整型变量
-miles = 1000.0 # 浮点型
-name = "John" # 字符串
+counter = 100  # 赋值整型变量
+miles = 1000.0  # 浮点型
+name = "John"  # 字符串
 
 print counter
 print miles
 print name
 
+
+#String 类型学习
+
+
+str = 'Hello World!'
+
+print str  # 输出完整字符串
+print str[0]  # 输出字符串中的第一个字符
+print str[2:5]  # 输出字符串中第三个至第五个之间的字符串
+print str[2:]  # 输出从第三个字符开始的字符串
+print str * 2  # 输出字符串两次
+print str + "TEST"  # 输出连接的字符串
+
+#List 类型学习
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+print list # 输出完整列表
+print list[0] # 输出列表的第一个元素
+print list[1:3] # 输出第二个至第三个的元素 
+print list[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2 # 输出列表两次
+print list + tinylist # 打印组合的列表
+
+#元组 类型学习
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+tinytuple = (123, 'john')
+
+print tuple # 输出完整元组
+print tuple[0] # 输出元组的第一个元素
+print tuple[1:3] # 输出第二个至第三个的元素 
+print tuple[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinytuple * 2 # 输出元组两次
+print tuple + tinytuple # 打印组合的元组
+
+
+
+#字典 类型学习
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print dict['one'] # 输出键为'one' 的值
+print dict[2] # 输出键为 2 的值
+print tinydict # 输出完整的字典
+print tinydict.keys() # 输出所有键
+print tinydict.values() # 输出所有值
 
 # raw_input("Press the enter key to exit.")
